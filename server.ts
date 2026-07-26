030d69da7d5188d3793a91595cafbabfa206194c
Ayesha-Mangi <aishamangi24@gmail.com>
2026-07-26 16:41:34 -0700

    Fix Render deployment

--- server.ts
+++ server.ts
@@ -19,7 +19,8 @@ const currentDir = getDirname();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  // const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json({ limit: '10mb' }));
 
